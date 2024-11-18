8347c17045bebd1c3748f77871cbfa562184c275
sohnsiyeon <yhm19850415@gmail.com>
2024-11-18 17:43:20 +0900

    Add all files

---
+++ src/main/java/PrincessMaker/PM_Spring/PmSpringApplication.java
@@ -0,0 +1,13 @@
+package PrincessMaker.PM_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PrincessMaker/PM_Spring/PmSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package PrincessMaker.PM_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
