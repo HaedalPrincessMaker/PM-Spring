00cc7e17eb378e5f10cc992dbeab2b8f97995a02
kdongsu5509 <kdongsu5509@gmail.com>
2024-11-23 16:33:13 +0900

    feat(cors) : sovle cors error

--- src/main/java/PrincessMaker/PM_Spring/WebConfig.java
+++ src/main/java/PrincessMaker/PM_Spring/WebConfig.java
@@ -1,6 +1,7 @@
 package PrincessMaker.PM_Spring;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new MyInterceptor())  // MyInterceptor 클래스 등록
                 .addPathPatterns("/**")  // 인터셉터가 적용될 URL 패턴
                 .excludePathPatterns("/login", "/register");  // 특정 URL은 제외
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // “*“같은 와일드카드를 사용
+                .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowCredentials(true); // 쿠키 인증 요청 허용
     }
 }
